94bc7b57f006dfd131bdabdd7e8c0d286d223727
Laxi64 <jerome2surtelephone@gmail.com>
2025-03-28 07:15:19 +0100

    Update Annoe.cs

--- Annoe.cs
+++ Annoe.cs
@@ -32,21 +32,30 @@ namespace annoeall
         public void notifall(Player player)
         {
             UIPanel panel = new UIPanel("ANNONCE!", UIPanel.PanelType.Input);
+            UIPanel panels = new UIPanel("Temps", UIPanel.PanelType.Input);
+            panels.AddButton("<color=#fc0303>FERMER</color>", delegate
+            {
+                    
+                player.ClosePanel(panels);
+               
+            });
             panel.AddButton("<color=#fc0303>FERMER</color>", delegate
             {
                     
                 player.ClosePanel(panel);
                
             });
-            panel.AddButton("<color=#00FF00>Envoyer</color>", delegate
+            panels.AddButton("<color=#00FF00>Envoyer</color>", delegate
             {
+                string time = panels.inputText;
+                float temps = float.Parse(time, System.Globalization.CultureInfo.InvariantCulture);
                 string content = panel.inputText;
                 if (player.IsAdmin)
                 {
                     
                     foreach (Player players in Nova.server.Players)
                     {
-                        players.Notify("ANNONCE", $"{content}", NotificationManager.Type.Warning , 10f);
+                        players.Notify("ANNONCE", $"{content}", NotificationManager.Type.Warning, temps);
                     }
                 }
                 else
@@ -54,15 +63,17 @@ namespace annoeall
                     player.Notify("ERREUR","Vous n'avez pas les droits administrateurs suffisant pour effectuer cette commande", NotificationManager.Type.Error);
                 }
             });
-           player.ShowPanelUI(panel); 
+            panel.AddButton("<color=#00FF00>Suivant</color>", delegate
+            { 
+                player.ClosePanel(panel);
+                player.ShowPanelUI(panels);
+            });
+            player.ShowPanelUI(panel);
         }
 
-       
-
-
-
-       public void annoe(Player player)
+        public void annoe(Player player)
         {
+            UIPanel panels = new UIPanel("Temps", UIPanel.PanelType.Input);
             UIPanel panel = new UIPanel("ANNONCE!", UIPanel.PanelType.Input);
             panel.AddButton("<color=#fc0303>FERMER</color>", delegate
             {
@@ -70,22 +81,37 @@ namespace annoeall
                 player.ClosePanel(panel);
                
             });
-            panel.AddButton("<color=#00FF00>Envoyer</color>", delegate
+            panels.AddButton("<color=#fc0303>FERMER</color>", delegate
             {
+                    
+                player.ClosePanel(panels);
+               
+            });
+            panel.AddButton("<color=#00FF00>Suivant</color>", delegate
+            {
+                player.ClosePanel(panel);
+                player.ShowPanelUI(panels); 
+            });
+            panels.AddButton("<color=#00FF00>Envoyer</color>", delegate
+            {
+                string time = panels.inputText;
+                float temps = float.Parse(time, System.Globalization.CultureInfo.InvariantCulture);
                 string content = panel.inputText;
                 if (player.IsAdmin)
                 {
                     
                     foreach (Player players in Nova.server.Players)
                     {
-                        players.setup.TargetShowCenterText("ANNONCE",$"{content}", 7f);
+                        players.setup.TargetShowCenterText("ANNONCE",$"{content}", temps);
                     }
                 }
                 else
                 {
-                 player.Notify("ERREUR","Vous n'avez pas les droits administrateurs suffisant pour effectuer cette commande", NotificationManager.Type.Error);
+                    player.Notify("ERREUR","Vous n'avez pas les droits administrateurs suffisant pour effectuer cette commande", NotificationManager.Type.Error);
                 }
             });
-            player.ShowPanelUI(panel); 
+            player.ShowPanelUI(panel);
+           
         }
     }
+}
